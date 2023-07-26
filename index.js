436f2ff4d124055659bcc9293490db04f920d88b
Elizabeth <ek@elizabethkillgore.com>
2023-07-26 16:46:22 -0500

    events listening events lab

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function addingEventListener() {const input = document.getElementById('button');
+// function addingEventListener() {const input = document.getElementById('button');
 
-function clickAlert() {
-  alert('I was clicked!');
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
 
-input.addEventListener('click', clickAlert);
-}
+// addingEventListener();
+
+
